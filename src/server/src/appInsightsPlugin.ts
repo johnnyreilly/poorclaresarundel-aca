3cf0be623ba4ee91c38be74a3edc168b007029d4
johnnyreilly <johnny_reilly@hotmail.com>
2025-01-06 08:10:35 +0000

    docs: plugin

--- src/server/src/appInsightsPlugin.ts
+++ src/server/src/appInsightsPlugin.ts
@@ -2,13 +2,14 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 import fp from 'fastify-plugin';
 import appInsights from 'applicationinsights';
 
-// based on https://github.com/microsoft/ApplicationInsights-node.js/issues/627#issuecomment-2194527018
 declare module 'fastify' {
     export interface FastifyRequest {
+        // here we augment FastifyRequest interface as advised here: https://fastify.dev/docs/latest/Reference/Hooks/#using-hooks-to-inject-custom-properties
         app: { start: number };
     }
 }
 
+// based on https://github.com/microsoft/ApplicationInsights-node.js/issues/627#issuecomment-2194527018
 export const appInsightsPlugin = fp(async (fastify: FastifyInstance, options: FastifyPluginOptions) => {
     if (!options.client) {
         console.log('App Insights not configured');
@@ -18,12 +19,15 @@ export const appInsightsPlugin = fp(async (fastify: FastifyInstance, options: Fa
     const client: appInsights.TelemetryClient = options.client;
     const urlsToIgnore = options.urlsToIgnore || [];
 
-    fastify.addHook('onRequest', async (request, _reply) => {
+    fastify.addHook('onRequest', async function (this: FastifyInstance, request, _reply) {
+        // const same = fastify === this;
+
+        // store the start time of the request
         const start = Date.now();
         request.app = { start };
     });
 
-    fastify.addHook('onResponse', async (request, reply) => {
+    fastify.addHook('onResponse', async function (this: FastifyInstance, request, reply) {
         if (urlsToIgnore.includes(request.raw.url)) return;
 
         const duration = Date.now() - request.app.start;
